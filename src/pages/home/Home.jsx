9f2d7ea17eb32cfc89307ff5110d2b5d58622ab5
manirisoa7 <manirisoa.jemima@gmail.com>
2023-08-30 16:20:49 +0300

    premier sauvegarde

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,31 +1,101 @@
 import React from "react";
 import './Home.css';
-import fte from "../../img/fte.png";
+import {Container, Row, Col} from "react-bootstrap";
+import { contactConfig } from "./content_option";
+import {FaFacebook} from "react-icons/fa6";
+import {FaInstagram} from "react-icons/fa6";
+import {FaLinkedin} from "react-icons/fa6";
+
 
 
 const Home = () => {
   return (
-    <div className="landing-page">
-      <div className="landing-header">
-        <div className="landing-header-logo">
-         <img src={fte} alt="" />
-        </div>
-       
-        <div className="landing-nav">
-          <ul>
-            <li><a href="/" className="nav-content">Programme</a></li>
-            <li><a href="/" className="nav-content">Reservation</a></li>
-            <li><a href="/" className="nav-content">Jeu et concours</a></li>
-            <li><a href="/" className="nav-content" >Contact</a></li>
+    <body>
+    <Container>
+      <Row className="sec_sp">
+        <Col lg='7' className="d-flex align items center">
+            
+          <form className="contact__form  w-100">
+            <Row>
+            <Row className ="mb-5 mt-3">
+              <Col lg='8'>
+                <div class="titre">
+                    <h1 className="display-4 mb-4">
+                       Contact
+                       <sub>Origami</sub>
+                    </h1>
+                </div>
+              </Col>
+            </Row>
 
-          </ul>
-        </div>
-      </div>
-      <div className="landing-content">
+              <Col lg='6' className="form-group">
+                <input 
+                  className="form-control"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  type="text"
+                />
+              </Col>
 
-      </div>
-    </div>
+              <Col lg='6' className="form-group">
+                <input 
+                  className="form-control rounded-0"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  type="email"
+                />
+              </Col>
+            </Row>
 
+            <textarea 
+                      className="form-control rounded-0"
+                      name="message" 
+                      id="message" 
+                      placeholder="Type your message..."
+                      rows="5">
+
+            </textarea>
+
+
+            <Row>
+              <Col lg='12' className="form-button">
+                 <button className="btn" type="submit">Send</button>
+              </Col>
+            </Row>
+
+          </form>
+        </Col>
+
+        <Col lg='5' className="mb-5">
+           
+      
+            <h3 className="color_sec py-4">Get in touch</h3>
+            <address>
+              <strong>Email : developer@gmail.com</strong>
+              <br />
+              <br />
+              <p>
+                <strong>Phone : +261 XX XXX XX</strong>
+              </p>
+            </address>
+            <p>{contactConfig.description}</p>
+            <p>{contactConfig.description}</p>
+        </Col>  
+      </Row>
+    
+            <div className="icon">
+                <FaFacebook/>facebook 
+                <span className="space"></span>
+                <FaInstagram/>instagram
+                <span className="space"></span>
+                <FaLinkedin/>linkedin</div>
+      <Row>
+
+      </Row>
+    </Container>
+    </body>
   );
 };
 
---
+++ src/pages/home/content_option.jsx
@@ -0,0 +1,8 @@
+const contactConfig ={
+    description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolor in at facilis sequi accusamus saepe maiores molestias, ea dolores reprehenderit deserunt unde odit omnis laborum earum magnam minima ipsum!'
+};
+
+export {
+    contactConfig
+};
