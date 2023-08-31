11c6e52930e1babd5d2ef357e0940cff03f4a507
Iandotiana <iandotiana08@gmail.com>
2023-08-31 09:12:11 +0300

    iando update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
-import "./pages/bootstrap/css/bootstrap.min.css";
+
 
 function App()
  {
